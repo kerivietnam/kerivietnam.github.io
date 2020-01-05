20951030a3d7d251e22a841e36b4381ecf585220
kerivietnam <49242771+kerivietnam@users.noreply.github.com>
2020-01-05 17:13:58 +0700

    qtsc_template3

---
+++ qtsc/template_html3/index7b17.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://localhost"/>
+	<id>http://localhost/animax/index.php</id>
+	<updated>2020-01-05T10:07:17+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://localhost/animax/animax/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ qtsc/template_html3/indexc0d0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Home</title>
+		<description><![CDATA[]]></description>
+		<link>http://localhost/animax/index.php</link>
+		<lastBuildDate>Sun, 05 Jan 2020 10:07:16 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://localhost/animax/animax/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
